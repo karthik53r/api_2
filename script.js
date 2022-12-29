32d96e4c915f21e9d6e5760e4b2595b348f8a811
karthik53r <118455908+karthik53r@users.noreply.github.com>
2022-12-29 11:15:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,26 +3,25 @@ var n1=" ";
 var n2 =" ";
 fetch("https://rawcdn.githack.com/kmitprojectschool/sem12022/788c820e40a750fecb30433c8d346457b7859abc/employee.json")
     .then((data) => {
-        //  console.log(data);
-        return data.json(); //converted to object
+        return data.json(); 
     })
     .then((objectData) => {
-        for (var i = 0; i < 3; i++) {
+        // console.log(objectData);
+        for (var i = 0; i < objectData.Employees.length; i++) {
             const obj = objectData.Employees[i];
             d += "<tr><td>" + obj.Name + "</td>";
             d += "<td>" + obj.Address + "</td>";
             d += "<td>" + obj.Salary + "</td>";
             d += "<td>" + obj.DateOfJoining + "</td></tr>";
-            document.querySelector("#table_body").innerHTML = d;
             if(obj.Salary > 50000){
-                n1+=obj.Name;
-                document.querySelector('#s').innerHTML=n1;
-                n1+=' , ';
+                n1+="<li>"+obj.Name+"</li>";
             }
+            
             if(obj.DateOfJoining[3] =='1' && obj.DateOfJoining[4] =='2'){
-                n2+=obj.Name;
-                document.querySelector('#m').innerHTML=n2;
-                n2+=' , ';
+                n2+="<li>"+obj.Name+"</li>";
             }
         }
+        document.querySelector("#table_body").innerHTML = d;
+        document.querySelector('#s').innerHTML=n1;
+        document.querySelector('#m').innerHTML=n2;
     });
